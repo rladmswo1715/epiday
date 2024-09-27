5584f50d909570589e5b575a839c3e30b493175b
eunjae <rladmswo1715@naver.com>
2024-09-27 22:44:48 +0900

    feat: 데이터 없을 시 UI 퍼블리싱

--- components/comment/CommentGroup.tsx
+++ components/comment/CommentGroup.tsx
@@ -1,12 +1,28 @@
+import Image from 'next/image';
 import CommentCard from './CommentCard';
+import noData from '@/public/images/icon/no-data.svg';
 
-const CommentGroup = () => {
+const CommentGroup = ({ commentsData }) => {
+  if (commentsData?.totalCount === 0) {
+    return (
+      <div className='mt-[4rem] flex flex-col items-center gap-[2.4rem] px-[16rem] py-[12.8rem]'>
+        <Image src={noData} alt='댓글 없음' width={144} height={144} />
+        <div>
+          <p className='text-center text-[2rem] text-var-black-600'>
+            아직 댓글이 없어요!
+            <br />
+            댓글을 달고 다른 사람들과 교류해보세요.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className='mt-[4rem]'>
-      <CommentCard />
-      <CommentCard />
-      <CommentCard />
-      <CommentCard />
+      {commentsData?.list.map((comment) => {
+        return <CommentCard cardData={comment} />;
+      })}
     </div>
   );
 };
