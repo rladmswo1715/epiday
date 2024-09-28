ffd4a93ac38d837a42743166539b7aad05c957c0
eunjae <rladmswo1715@naver.com>
2024-09-28 23:21:35 +0900

    feat: 댓글 수정 기능 구현

--- api/comments.ts
+++ api/comments.ts
@@ -1,5 +1,13 @@
 import BASE_URL from '@/constant/url';
 
+type CommentData = {
+  isPrivate: boolean;
+  content: string;
+};
+
+type PatchCommentData = CommentData & {
+  commentId: number;
+};
 export const getEpidayCommentsById = async (id: number, accessToken: string, limit = 5) => {
   try {
     const response = await fetch(`${BASE_URL}/epigrams/${id}/comments?limit=${limit}`, {
@@ -20,12 +28,6 @@ export const getEpidayCommentsById = async (id: number, accessToken: string, lim
   }
 };
 
-type CommentData = {
-  epigramId: number;
-  isPrivate: boolean;
-  content: string;
-};
-
 export const postAddComment = async (commentData: CommentData, accessToken: string) => {
   try {
     const response = await fetch(`${BASE_URL}/comments`, {
@@ -42,6 +44,32 @@ export const postAddComment = async (commentData: CommentData, accessToken: stri
 
       if (errorData.message === 'jwt expired') throw { message: '토큰 유효기간 만료' };
       throw { message: errorData.message || '에피데이 댓글 등록 실패', details: errorData.details };
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const patchUpdateComment = async (commentData: PatchCommentData, accessToken: string) => {
+  try {
+    const { commentId, ...dataToPatch } = commentData;
+
+    const response = await fetch(`${BASE_URL}/comments/${commentId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(dataToPatch),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+
+      if (errorData.message === 'jwt expired') throw { message: '토큰 유효기간 만료' };
+      throw { message: errorData.message || '에피데이 댓글 삭제 실패', details: errorData.details };
     }
 
     return await response.json();
--- components/comment/CommentCard.tsx
+++ components/comment/CommentCard.tsx
@@ -1,12 +1,59 @@
 import { timeAgo } from '@/utils/formatDate';
 import ProfileImage from '../ProfileImage';
 import { useModalStore } from '@/store/modalStore';
+import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import VisibilityToggle from '../VisibilityToggle';
+import { addCommentSchema } from '@/schema/addCommentSchema';
+import { useSession } from 'next-auth/react';
+import { patchUpdateComment } from '@/api/comments';
+
+type TPatchCommentData = {
+  commentId: number;
+  isPrivate: boolean;
+  content: string;
+};
 
 const CommentCard = ({ cardData }) => {
   const { openModal } = useModalStore();
+  const [isEditing, setIsEditing] = useState(false);
+  const [contentValue, setContentValue] = useState(cardData?.content);
+  const [isVisible, setIsVisible] = useState(cardData?.isPrivate);
+  const { data: session } = useSession();
+
+  const updateCommentMutation = useMutation({
+    mutationFn: async (patchCommentData: TPatchCommentData) => {
+      await patchUpdateComment(patchCommentData, session.accessToken);
+    },
+    onSuccess: () => {
+      //queryClient.invalidateQueries({ queryKey: ['epiday', 'comments', modalProps.epidayId] });
+    },
+    onSettled: () => {
+      setIsEditing(false);
+    },
+  });
 
   const handleConfirmDelete = () => {
     openModal('confirmDelete', { commentId: cardData.id, epidayId: cardData.epigramId });
+  };
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveClick = async () => {
+    addCommentSchema.parse(contentValue);
+    const patchCommentData = { commentId: cardData.id, isPrivate: isVisible, content: contentValue };
+    updateCommentMutation.mutate(patchCommentData);
+  };
+
+  const handleSaveCancelClick = () => {
+    setIsEditing(false);
+    setContentValue(cardData.content);
+  };
+
+  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContentValue(e.target.value);
   };
 
   return (
@@ -19,13 +66,40 @@ const CommentCard = ({ cardData }) => {
             <span>{timeAgo(cardData?.createdAt)}</span>
           </p>
           <div className='flex gap-[1.6rem] text-[1.6rem] leading-[1.8rem] underline-offset-[0.2rem]'>
-            <button className='text-var-black-600 underline decoration-var-black-600'>수정</button>
-            <button className='text-var-error underline decoration-var-error' onClick={handleConfirmDelete}>
-              삭제
-            </button>
+            {isEditing ? (
+              <>
+                <button className='text-var-black-600 underline decoration-var-black-600' onClick={handleSaveClick}>
+                  저장
+                </button>
+                <button className='text-var-black-600 underline decoration-var-black-600' onClick={handleSaveCancelClick}>
+                  취소
+                </button>
+              </>
+            ) : (
+              <>
+                <button className='text-var-black-600 underline decoration-var-black-600' onClick={handleEditClick}>
+                  수정
+                </button>
+                <button className='text-var-error underline decoration-var-error' onClick={handleConfirmDelete}>
+                  삭제
+                </button>
+              </>
+            )}
           </div>
         </div>
-        <p className='mt-[1.6rem] whitespace-pre-line break-words text-[2rem] text-var-black-700'>{cardData?.content}</p>
+        {isEditing ? (
+          <>
+            <textarea
+              className='mt-[1.6rem] w-[52.8rem] overflow-scroll rounded-[0.8rem] border-[0.1rem] border-var-line-200 px-[1.6rem] py-[1.2rem] text-[2rem] scrollbar-hide'
+              value={contentValue}
+              onChange={handleCommentChange}
+              maxLength={100}
+            />
+            <VisibilityToggle isVisible={isVisible} setIsVisible={setIsVisible} />
+          </>
+        ) : (
+          <p className='mt-[1.6rem] whitespace-pre-line break-words text-[2rem] text-var-black-700'>{cardData?.content}</p>
+        )}
       </div>
     </div>
   );
