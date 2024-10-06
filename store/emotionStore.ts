018afba145c764ffadf3dd8dd5f101ef90b97723
eunjae <rladmswo1715@naver.com>
2024-10-06 15:46:29 +0900

    feat: emotion zustand 관리

---
+++ store/emotionStore.ts
@@ -0,0 +1,48 @@
+import { getTodayEmotion, postTodayEmotion } from '@/api/emotion';
+import { useSession } from 'next-auth/react';
+import { create } from 'zustand';
+
+type TEmotion = 'MOVED' | 'HAPPY' | 'WORRIED' | 'SAD' | 'ANGRY' | null;
+
+type TResponseEmotion = {
+  id: number;
+  userId: number;
+  emotion: string;
+  createdAt: string;
+};
+
+interface IEmotionStore {
+  selectedEmotion: TEmotion;
+  setSelectedEmotion: (emotion: TEmotion) => void;
+  postEmotion: (accessToken: string) => Promise<void>;
+  getEmotion: (userId: string) => Promise<TResponseEmotion>;
+}
+
+export const useEmotionStore = create<IEmotionStore>((set) => ({
+  selectedEmotion: null,
+  setSelectedEmotion: (emotion) => set({ selectedEmotion: emotion }),
+  postEmotion: async (accessToken) => {
+    const { selectedEmotion } = useEmotionStore.getState();
+
+    if (selectedEmotion) {
+      const postData = {
+        emotion: selectedEmotion,
+      };
+      try {
+        await postTodayEmotion(postData, accessToken);
+      } catch (error) {}
+    }
+  },
+  getEmotion: async (userId) => {
+    try {
+      if (!userId) return;
+
+      const result = await getTodayEmotion(userId);
+      if (result) {
+        return result;
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+}));
