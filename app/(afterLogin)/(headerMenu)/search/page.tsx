665226bed37e070ed72018c476ce30e8319af9b2
eunjae <rladmswo1715@naver.com>
2024-10-30 13:08:21 +0900

    fix: 빌드 에러 수정

--- app/(afterLogin)/(headerMenu)/search/page.tsx
+++ app/(afterLogin)/(headerMenu)/search/page.tsx
@@ -1,11 +1,14 @@
 import SearchContainer from '@/components/search/SearchContainer';
 import InnerLayout from '@/components/InnerLayout';
+import { Suspense } from 'react';
 
 const Search = () => {
   return (
     <div className='bg-var-blue-100'>
       <InnerLayout>
-        <SearchContainer />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchContainer />
+        </Suspense>
       </InnerLayout>
     </div>
   );
